3c342e807bce1514d4bcb838ca8ffa5cb2b2ed32
Tetiana <tetiana.martynchuk@gmail.com>
2023-11-01 21:24:46 +0200

    Completed homework 16: Playwright actions and assertions

--- playwright.config.js
+++ playwright.config.js
@@ -25,15 +25,19 @@ const config = defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    headless: false,
     httpCredentials: {
       username: 'guest',
-      password: 'superstrongpassword'
+      password: 'welcome2qauto'
     },
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://qauto.forstudy.space',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    launchOptions:{
+      slowMo: 1000
+    }
   },
 
   /* Configure projects for major browsers */
